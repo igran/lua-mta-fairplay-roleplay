d27329e1c23e353e3cd28a0b2b1538ca6b0ea252
Socialz <socialz.fin@gmail.com>
2015-02-15 15:34:15 +0200

    items: fixed breakable attribute for worlditems

--- items/client/worlditems.lua
+++ items/client/worlditems.lua
@@ -25,7 +25,7 @@
 addEventHandler( "onClientResourceStart", resourceRoot,
 	function( )
 		for _, object in ipairs( getElementsByType( "object" ) ) do
-			if ( getElementData( object, "worlditem.id" ) ) then
+			if ( getElementData( object, "worlditem:id" ) ) then
 				setObjectBreakable( object, false )
 			end
 		end
